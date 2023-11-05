f6a72090f809b4001b52fe1afd59fed026dcc958
brianabeltimothy <brianabeltimothyd@gmail.com>
2023-11-05 23:25:31 +0000

    add users controller

--- API/Data/DataContext.cs
+++ API/Data/DataContext.cs
@@ -1,11 +1,8 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Entities;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data;
+
 public class DataContext : DbContext
 {
     public DataContext(DbContextOptions options) : base(options)
